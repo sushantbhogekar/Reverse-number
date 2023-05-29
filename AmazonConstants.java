a78247d7bf49616bdd71729c8cb354d29736ae90
sushantbhogekar <134822898+sushantbhogekar@users.noreply.github.com>
2023-05-29 13:11:07 +0530

    Add files via upload

---
+++ AmazonConstants.java
@@ -0,0 +1,12 @@
+package com.amazon.constans;
+
+public class AmazonConstants {
+//User Credentials
+  public static String userName = "9923252274"; 
+  public static String pass = "Sanket@123"; 
+  public static String url = "https://www.amazon.in/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26signIn%3D1%26useRedirectOnSuccess%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"; 
+  
+//Error Messages
+  public static String invalidPhoneNumberErrorMessage = "Incorrect phone number";
+  public static String incorrectUserName = "1234"; 
+}
